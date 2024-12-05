63e0e6249b4db1184d16d29f632a458ebe605490
codling_iy maker <118177032+crazystuffmaker@users.noreply.github.com>
2024-12-05 16:13:23 +0300

    Add files via upload

---
+++ console (3).go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/lithammer/dedent"
+	"github.com/crazystuffmaker/pjsekai-overlay-iforgot/pkg/pjsekaioverlay"
+)
+
+func Title() {
+	fmt.Printf(
+		strings.TrimSpace(dedent.Dedent(`
+      %s== pjsekai-overlay -----------------------------------------------------------%s
+        %spjsekai-overlay / プロセカ風動画作成補助ツール%s
+        Version: %s%s%s
+        Developed by %s名無し｡(@sevenc-nanashi)%s
+        Modified by: uh %scrazystuffmaker%s ya
+        https://github.com/sevenc-nanashi/pjsekai-overlay
+        https://github.com/crazystuffmaker/pjsekai-overlay-iforgot
+      %s-------------------------------------------------------------------------------%s
+    `))+"\n\n",
+		RgbColorEscape(0x00afc7), ResetEscape(),
+		RgbColorEscape(0x00afc7), ResetEscape(),
+		RgbColorEscape(0x0f6ea3), pjsekaioverlay.Version, ResetEscape(),
+		RgbColorEscape(0x48b0d5), ResetEscape(),
+                RgbColorEscape(0xa36ceb), ResetEscape(),
+		RgbColorEscape(0xff5a91), ResetEscape(),
+	)
+
+}
+
+func RgbColorEscape(rgb int) string {
+	return fmt.Sprintf("\033[38;2;%d;%d;%dm", (rgb>>16)&0xff, (rgb>>8)&0xff, rgb&0xff)
+}
+
+func AnsiColorEscape(color int) string {
+	return fmt.Sprintf("\033[38;5;%dm", color)
+}
+
+func ResetEscape() string {
+	return "\033[0m"
+}
